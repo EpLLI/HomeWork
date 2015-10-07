1b2e79df30de5eb5a13d06eb3a7c0b0453fa60f7
EpLLI <staniush.iv@gmail.com>
2015-10-07 14:13:12 +0200

    First

---
+++ Den.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;// инициализация ввода данных
+public class Den {
+	public static void main(String[] args) {
+		int a;
+		Scanner in = new Scanner(System.in);
+		a= in.nextInt();
+		switch (a) {               				//задаём выражение свича
+		  case 1:								//аргументы					
+		  System.out.println("Понедельник");
+		  break;
+		  case 2:
+		  System.out.println("Вторник");
+		  break;
+		  case 3:
+		  System.out.println("Среда");
+		  break;
+		  case 4:
+		  System.out.println("Четверг");
+		  break;
+		  case 5:
+		  System.out.println("Пятница");
+		  break;
+		  case 6:
+		  System.out.println("Субота");
+		  break;
+		  case 7:
+		  System.out.println("Воскресение");
+		  break;
+		  default:								//при не существующем
+		  System.out.println("Дня с таким номером не существует");
+		}
+
+	}
+
+}
---
+++ Rubl.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;// инициализация ввода данных
+public class Rubl {
+	public static void main(String[] args) {
+		int a;
+		Scanner in = new Scanner(System.in);
+		a= in.nextInt();
+		if (a%100>10 && a%100<15){					//условие исключений
+			System.out.println(a+" Рублей");}
+			else if (a%10==1){ 						//условие 1 группы цифр
+			System.out.println(a+" Рубль");}
+			else if (a%10==2)						//условие 2 группы цифр
+		{ 
+			System.out.println(a+" Рубля"); 
+		}
+			else if (a%10==3)						//условие 2 группы цифр
+		{ 
+			System.out.println(a+" Рубля"); 
+		}
+			else if (a%10==4)						//условие 2 группы цифр
+		{ 
+			System.out.println(a+" Рубля"); 
+		}	
+		else {
+			System.out.println(a+" Рублей");
+		}
+		}
+}
