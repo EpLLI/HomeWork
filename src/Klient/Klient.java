2652e7ac53dc35210e9f21a42e416562d1645738
EpLLI <staniush.iv@gmail.com>
2015-11-08 14:27:09 +0200

    Ispravlenie

--- src/Klient/Klient.java
+++ src/Klient/Klient.java
@@ -1,12 +1,8 @@
-/**
- * Class works with the client value
- * Its values Serializable
- */
 package Klient;
 
+import java.io.Serializable;
 
-
-protected class Klient extends Human {
+public class Klient extends Human implements Serializable {
 	private String id;
 
 	public Klient(String name, int pin, String id) {
@@ -22,7 +18,7 @@ protected class Klient extends Human {
 	public void setId(String id) {
 		this.id = id;
 	}
-	//output method of data about the user
+
 	@Override
 	public String toString() {
 		StringBuilder klientSchet = new StringBuilder(super.toString() + " Id: " + id);
