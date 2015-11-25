0b056dc904502099c5ca9301b5eb754124a80aa1
EpLLI <staniush.iv@gmail.com>
2015-11-25 13:42:17 +0200

    Ocered

---
+++ src/Zadanie13_1/Ocered.java
@@ -0,0 +1,37 @@
+package Zadanie13_1;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Ocered {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		ExecutorService kas = Executors.newFixedThreadPool(3);
+		for (int i = 0; i < 10; i++) {
+			Runnable kli = new Kli(counter++);
+			kas.execute(kli);
+		}
+		kas.shutdown();
+	}
+}
+
+class Kli implements Runnable {
+	private int counter;
+	String[] items = { "Chai", "Bulocka", "Sup" };
+
+	public Kli(int counter) {
+		this.counter = counter;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Kassa " + counter + " in");
+		int k = 0;
+		String name = Thread.currentThread().getName();
+		for (int i = 0; i < items.length; i++) {
+			System.out.println("Klient " + counter + " Kassa " + name.substring(name.length() - 1) + ": " + items[i]);
+		}
+		System.out.println("Klient " + counter + " out");
+		System.out.println("Kassa svobodna");
+	}
+}
