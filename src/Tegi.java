3f73a690ec53fa4326ee5afe512b1c54711562e3
EpLLI <staniush.iv@gmail.com>
2015-11-29 20:14:52 +0200

    Теги

--- src/Tegi.java
+++ src/Tegi.java
@@ -1,9 +1,9 @@
 public class Tegi {
 
 	public static void main(String[] args) {
-		String str="ololl <p id=p1> ololoF <p id=p2> olol <p id=p1> <p id>o  <p>oljg";//строка пример
+		String str="ololl <p id=p1> ololoF <p id=p2> <tt>olol <p id=p1> <p id>o  <p>oljg";//строка пример
 		 System.out.println(str);
-        System.out.println(str.replaceAll("<.*?>", "<p>"));//замена регулярным вырожением 
+        System.out.println(str.replaceAll("<p"+".*?>", "<p>"));//замена регулярным вырожением 
        
         }
 	}
